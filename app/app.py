9cacd0f47c0236901669050db73e0591ec1209c3
Adrien Dimitri <amaugueret@gmail.com>
2024-01-20 14:13:24 +0100

    implemented global var for test_samples and added route to get them

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, g, session
 from app.sms_spam_detector import SpamDetector
+import random
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 @app.route('/')
 def index():
@@ -17,8 +19,28 @@ def train():
     spam_detector.preprocess(corpus)
     spam_detector.train_test_split(train_percentage)
     spam_detector.auto_train(feature_extraction)
+    test_samples = random.sample(corpus, 6)
+    test_samples = [sample.split('\t')[1] for sample in test_samples]
+    session['test_samples'] = test_samples
 
     return jsonify({'message': 'Training completed successfully'})
+
+@app.route('/classify', methods=['GET'])
+def classify():
+    test_samples = session.get('test_samples', [])
+    test_sample_message = test_samples.pop()
+
+    return jsonify({'sample_text': test_sample_message})
+
+# get a random message to display on the web page
+@app.route('/get_random_text', methods=['GET'])
+def get_random_text():
+    test_samples = session.get('test_samples', [])
+    test_sample_message = test_samples.pop()
+    print(test_sample_message)
+
+    return jsonify({'sample_text': test_sample_message})
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
